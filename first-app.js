fbc19b71cab78a0e9ef0a1b303c8064ca09cd332
Istiak Hossain Tushar <47757323+Istiak-Tushar42@users.noreply.github.com>
2019-07-28 11:00:03 +0600

    Arrow Function

--- first-app.js
+++ first-app.js
@@ -1,1 +1,4 @@
 console.log('Hello from node js.');
+
+const fs = require('fs'); // fs - file system
+fs.writeFileSync('hello.txt', 'Hello from Node.js');
---
+++ play.js
@@ -0,0 +1,35 @@
+// Play with Basics
+
+// Variables - var, let, const
+// Next gen JavaScript use let
+// var & let are similer
+// const contain fixed values & it must have a value
+
+const name = 'Max';
+let age = 26;
+const hasHobbies = true;
+
+age = 30;
+
+// ARROW FUNCTION => Also called ANONYMOUS FUNCTION BECSUSE IS'S HAVE NO NAME
+const summarizeUser = (userName, userAge, userHasHobby) => {
+    return ('Name is ' + userName + ', age is ' + userAge + ' and user has hobbies: ' + userHasHobby);
+};
+
+// Output
+console.log(summarizeUser(name, age, hasHobbies));
+
+// Named Function Shortcut
+// IF we need to return only one statement we can write it as one line
+// const add = (a, b) => a + b;
+
+// const addOne = a => a + 1;
+
+// IF We have no argument then we have specify the ()
+const addRandom = () => 1 + 2;
+
+
+// Output
+// console.log(add(2, 2));
+// console.log(addOne(1));
+console.log(addRandom());
